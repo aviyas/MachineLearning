403f18ca9c56daba7ab5db0fa60e42b213a86ba9
Aviya Sela <aviya.sela@post.idc.ac.il>
2016-06-12 15:02:43 +0300

    Implemented initializeCentroids, findKMeansCentroids and findClosestCentroid

--- MLHW07 - K-Means/hw7/KMeans.java
+++ MLHW07 - K-Means/hw7/KMeans.java
@@ -1,40 +1,55 @@
 package hw7;
 
+import weka.core.Debug;
 import weka.core.Instances;
 import weka.core.Instance;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Random;
 
 public class KMeans {
 
     int k;
-    double[] centroids;
+    double[][] centroids;
 
     public void setK(int k) {
         this.k = k;
     }
 
     /**
-     * Initializes any parameters (therefore should call initializecentroids)
-     * and runs the K-Means algorithm (which means to call findKMeansCentroids methods).
-     *
+     * Initializes any parameters and runs the K-Means algorithm.
      * It runs everything necessary to find the K clusters centroids on the input instances.
      *
      * @param datset - to run the algorithm on.
      */
     public void buildClusterModel(Instances datset) {
-
         initializeCentroids(datset);
         findKMeansCentroids(datset);
-
     }
 
 
     /**
-     * Initializes the centroids by selecting k random instances from the training set and setting the centers as those instances.
+     * Initializes the centroids by selecting k random instances from the training set
+     * and setting the centers as those instances.
      * @param dataset - the training set.
      */
     public void initializeCentroids(Instances dataset) {
+        centroids = new double[k][4];
 
+        Random random = new Random();
+        int range = dataset.numAttributes() + 1;
+        Instance randomInstance;
+
+        // Randomizes k instances
+        for (int i = 0; i < k; i++) {
+            randomInstance = dataset.instance(random.nextInt(range));
+
+            // Copies their RGBA values
+            for (int j = 0; j < 4; j++) {
+                centroids[i][j] = randomInstance.value(j);
+            }
+        }
     }
 
     /**
@@ -47,6 +62,51 @@ public class KMeans {
      */
     public void findKMeansCentroids(Instances dataset) {
 
+        for (int i = 0; i < 40; i++) {
+            findKMeansCentroidsIteration(dataset);
+        }
+
+    }
+
+    public void findKMeansCentroidsIteration(Instances dataset) {
+
+        ArrayList<Instance>[] clusters = (ArrayList<Instance>[])new ArrayList[k];
+
+        // 1. Assigns all instances to their nearest centroid
+        int closestCentroid;
+        Instance curInstance;
+
+        for (int i = 0; i < dataset.numInstances(); i++) {
+            curInstance = dataset.instance(i);
+            closestCentroid = findClosestCentroid(curInstance);
+
+            // Adds the instance to the cluster of its closest centroids'
+            clusters[closestCentroid].add(curInstance);
+        }
+
+        // 2. Recomputes centroids using their assigned cluster
+        double[] newCentroid = new double[4];
+
+        // For each centroid,
+        for (int centroid = 0; centroid < k; centroid++) {
+
+            // Iterates through the instances of its cluster and sums their RGBA values
+            ArrayList<Instance> cluster = clusters[centroid];
+
+            for (Instance instance : cluster) {
+                for (int i = 0; i < 4; i++) {
+                    newCentroid[i] += instance.value(i);
+                }
+            }
+
+            // Divides by the number of instances in the cluster to get the average
+            for (int i = 0; i < 4; i++) {
+                newCentroid[i] /= cluster.size();
+            }
+
+            // sets the new centroid that was found
+            centroids[centroid] = newCentroid;
+        }
     }
 
 
@@ -56,7 +116,7 @@ public class KMeans {
      * @param centroid - the array of centroids.
      * @return the squared euclidian distance between the instance to the centroid.
      */
-    public double calcSquaredDistance(Instance instance, double centroid) {
+    public double calcSquaredDistance(Instance instance, double[] centroid) {
 
 
         return -1.0;
@@ -70,8 +130,16 @@ public class KMeans {
      */
     public int findClosestCentroid(Instance instance) {
 
+        double currentDistance;
+        double minDistance = Integer.MAX_VALUE;
+        int minCentroid = 0;
 
-        return -1;
+        for (int curCentroid = 0; curCentroid < k; curCentroid++) {
+            currentDistance = calcSquaredDistance(instance, centroids[curCentroid]);
+            minCentroid = (currentDistance < minDistance) ? curCentroid : minCentroid;
+        }
+
+        return minCentroid;
     }
 
     /**
